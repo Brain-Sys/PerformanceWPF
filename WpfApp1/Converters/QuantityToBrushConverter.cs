ba9af906d7f58f45e6b44f7eab141408433a334c
Damiani Liborio Igor <igor.damiani@hotmail.com>
2018-01-26 09:04:57 +0100

    Add project files.

---
+++ WpfApp1/Converters/QuantityToBrushConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfApp1.Converters
+{
+    public class QuantityToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double v = (double)value;
+
+            if (v == 0)
+            {
+                return new SolidColorBrush(Colors.Transparent);
+            }
+            else if (v < 1000)
+            {
+                return Application.Current.Resources["low"];
+            }
+            else if (v >= 1000 && v <= 3000)
+            {
+                return Application.Current.Resources["medium"];
+            }
+            else
+            {
+                return Application.Current.Resources["high"];
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
---
+++ WpfApp1/MainViewModel.cs
@@ -0,0 +1,72 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.ObjectModel;
+using System.Timers;
+using System.Windows;
+using Teleta.Bari.XF.Repository;
+
+namespace WpfApp1
+{
+    public class MainViewModel : ViewModelBase
+    {
+        Random rnd = new Random((int)DateTime.Now.Ticks);
+        Timer timer1;
+        Timer timer2;
+
+        public RelayCommand StartCommand { get; set; }
+        public ObservableCollection<Article> Articles { get; set; }
+
+        public MainViewModel()
+        {
+            this.StartCommand = new RelayCommand(StartCommandExecute);
+            this.Articles = new ObservableCollection<Article>();
+            load();
+        }
+
+        private void load()
+        {
+            int count = 100000;
+            for (int i = 0; i < count; i++)
+            {
+                Article a = new Article();
+                a.ID = rnd.Next(1, 1000);
+                //a.Quantity = rnd.Next(87, 191);
+                a.Name = string.Concat("Nome Art. ", a.ID);
+
+                this.Articles.Add(a);
+            }
+
+            base.RaisePropertyChanged(nameof(Articles));
+        }
+
+        private void StartCommandExecute()
+        {
+            timer1 = new Timer(100);
+            timer1.Elapsed += Timer_Elapsed1;
+
+            timer2 = new Timer(1000);
+            timer2.Elapsed += Timer_Elapsed2;
+
+            timer1.Start();
+            timer2.Start();
+        }
+
+        private void Timer_Elapsed2(object sender, ElapsedEventArgs e)
+        {
+            Article a = new Article();
+            a.ID = rnd.Next(1, 1000);
+            a.Name = string.Concat("Nome Art. ", a.ID);
+
+            Application.Current.Dispatcher.Invoke(() => { this.Articles.Add(a); });
+        }
+
+        private void Timer_Elapsed1(object sender, ElapsedEventArgs e)
+        {
+            for (int i = 0; i < this.Articles.Count; i++)
+            {
+                this.Articles[i].Quantity = rnd.Next(0, 4000);
+            }
+        }
+    }
+}
